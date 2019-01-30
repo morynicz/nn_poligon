34a7fa5a9bca67d8973d2236b4f0e205efdbd682
Michał Orynicz <morynicz@gmail.com>
2019-01-30 08:14:18 +0100

    Managed to use dataset with batching and iterator, broken verification

--- first_tf_cnn.py
+++ first_tf_cnn.py
@@ -19,51 +19,52 @@ def main():
     x_test = preprocess_input(x_test_r)
     y_test = preprocess_output(num_classes, y_test_r)
 
-    x = tf.placeholder(dtype="float", shape=[x_train.shape[0], None], name="input")
-    y = tf.placeholder(dtype="float", shape=[y_train.shape[0], None], name="output")
+    x_placeholder = tf.placeholder(dtype="float", shape=[x_train.shape[0], None], name="input")
+    y_placeholder = tf.placeholder(dtype="float", shape=[y_train.shape[0], None], name="output")
 
     seed = 5
 
     # x_d = tf.data.Dataset.from_tensors()
     # y_d = tf.data.Dataset.from_tensors()
-    d = tf.data.Dataset.from_tensor_slices((tf.transpose(x_train), tf.transpose(y_train)))
-    print(d)
-    d_b = d.shuffle(buffer_size=128).batch(512, False)
-    it = d_b.make_one_shot_iterator().get_next()
+    # d = tf.data.Dataset.from_tensor_slices((tf.transpose(x_train), tf.transpose(y_train)))
+    # print(d)
+    # d_b = d.shuffle(buffer_size=128).batch(512, False)
+    # it = d_b.make_one_shot_iterator().get_next()
     # print(it)
     # for xit, yit in it:
         # print(xit)
     # print(d_b)
-    # batches = generate_batches(x_train, y_train, 1, seed)
-    # return
-
     with tf.name_scope('hyper_parameters'):
 
         learning_rate = 0.001
         beta = 0.001
-        num_epochs = 10
+        num_epochs = 200
+        batch_size = 60000
 
         initializer = tf.contrib.layers.xavier_initializer(seed=seed)
 
         layer_params = [
-            (400, tf.nn.relu, "1"),
-            (200, tf.nn.relu, "2"),
-            (100, tf.nn.relu, "3"),
-            (10, identity, "4")
+            (784, 400, tf.nn.relu, "1"),
+            (400, 200, tf.nn.relu, "2"),
+            (200, 100, tf.nn.relu, "3"),
+            (100, 10, identity, "4")
         ]
+
+        iter = generate_batches_iterator(tf.transpose(x_placeholder), tf.transpose(y_placeholder), batch_size, seed, num_epochs)
+        x, y = iter.get_next()
 
         tf.summary.scalar('number of epochs', num_epochs)
         tf.summary.scalar('learning rate', learning_rate)
         tf.summary.scalar('beta', beta)
 
-        for n_outputs, activation, name_postfix in layer_params:
+        for n_inputs, n_outputs, activation, name_postfix in layer_params:
             tf.summary.scalar("numer of hidden units in layer {}".format(name_postfix), n_outputs)
 
-    A, W, B = build_fc_layers(initializer, layer_params, x)
-
+    A, W, B = build_fc_layers(initializer, layer_params, tf.transpose(x))
+    print(A[-1].shape)
     with tf.name_scope("Costs"):
         cost = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits_v2(logits=tf.transpose(A[-1]), labels=tf.transpose(y)))
+            tf.nn.softmax_cross_entropy_with_logits_v2(logits=tf.transpose(A[-1]), labels=y))
 
         regularized_cost = tf.reduce_mean(cost + beta * tf.contrib.layers.apply_regularization(tf.nn.l2_loss, W))
         tf.summary.histogram("regularized cost", regularized_cost)
@@ -86,33 +87,40 @@ def main():
         merged = tf.summary.merge_all()
 
         session.run(init)
-
         start = datetime.datetime.now()
 
         for i in range(num_epochs):
-            # batches = generate_batches(x_train, y_train, 1, seed)
-            x_batch, y_batch = it
-            print(x_batch)
-            print(y_batch)
-            summary, _, epoch_cost = session.run([merged, optimizer, cost], feed_dict={x: x_batch, y: y_batch})
-            writer.add_summary(summary, i)
+            session.run(iter.initializer, feed_dict={x_placeholder: x_train, y_placeholder: y_train})
+            epoch_cost = 0
+            while True:
+
+                try:
+                    summary, _, cost_value = session.run([merged, optimizer, cost])
+                    writer.add_summary(summary, i)
+                    epoch_cost += cost_value
+                    # xp, yp = session.run([x, y])
+                    # print("x {} y {}".format(xp.shape, yp.shape))
+                except tf.errors.OutOfRangeError:
+                    break
+                except Exception as e:
+                    print("dupa" + str(e))
             costs.append(epoch_cost)
             if i % 10 == 0:
                 mean_time = (datetime.datetime.now() - start) / 10
                 print("epoch {} cost: {} mean time {}".format(i, epoch_cost, mean_time))
                 start = datetime.datetime.now()
 
-        train_accuracy_val, summary = session.run([train_accuracy, merged], feed_dict={x: x_train, y: y_train})
-        writer.add_summary(summary)
-        test_accuracy_val, summary = session.run([test_accuracy, merged], feed_dict={x: x_test, y: y_test})
-        writer.add_summary(summary)
-
-        print("Train accuracy = {train} (e={train_error})\nTest accuracy = {test} (e={test_error})".format(
-            train=train_accuracy_val,
-            train_error=(1-train_accuracy_val),
-            test=test_accuracy_val,
-            test_error=(1-test_accuracy_val)))
-
+        # train_accuracy_val, summary = session.run([train_accuracy, merged], feed_dict={x_placeholder: x_train, y_placeholder: y_train})
+        # writer.add_summary(summary)
+        # test_accuracy_val, summary = session.run([test_accuracy, merged], feed_dict={x_placeholder: x_test, y_placeholder: y_test})
+        # writer.add_summary(summary)
+        #
+        # print("Train accuracy = {train} (e={train_error})\nTest accuracy = {test} (e={test_error})".format(
+        #     train=train_accuracy_val,
+        #     train_error=(1-train_accuracy_val),
+        #     test=test_accuracy_val,
+        #     test_error=(1-test_accuracy_val)))
+        #
         plt.plot(np.squeeze(costs))
         plt.ylabel('cost')
         plt.xlabel('iterations')
@@ -123,21 +131,44 @@ def main():
         writer.close()
 
 
-def generate_batches(x_train, y_train, num_batches, seed):
-    print(x_train)
-    zipped = [el for el in zip(x_train, y_train)]
-    random.seed(seed)
-    shuffled = random.sample(zipped, len(zipped))
-    print(shuffled)
-    return [(x_train, y_train)]
+def generate_batches_iterator(x_train, y_train, batch_size, seed, num_of_epochs):
+    # print(x_train)
+    # # zipped = [el for el in zip(x_train, y_train)]
+    # print(x_train.shape)
+    # print(y_train.shape)
+    # x_t = tf.transpose(x_train)
+    # y_t = tf.transpose(y_train)
+    # print(x_t)
+    # print(y_t)
+    #
+    # def compose(e):
+    #     print("x: {} y: {}".format(e[0].shape, e[1].shape))
+    #     return e[0], e[1]
+    #
+    # zipped = tf.map_fn(lambda e: (e[0],e[1]), (x_t, y_t))
+    #
+    # # zipped = [(x,y) for x in x_t for y in y_t]
+    # print(len(zipped))
+    # random.seed(seed)
+    # shuffled = random.sample(zipped, len(zipped))
+    # # print(shuffled)
+
+    d = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+
+    def transformation(x,y):
+        return tf.transpose(x), tf.transpose(y)
+
+    d_b = d.map(transformation).repeat(1).shuffle(buffer_size=128, seed=seed).batch(batch_size=batch_size)
+    print(d_b)
+    return d_b.make_initializable_iterator()
 
 
 def build_fc_layers(initializer, layer_params, x):
     A, W, B = [], [], []
     a_prev = x
-    for n_outputs, activation, name_postfix in layer_params:
+    for n_inputs, n_outputs, activation, name_postfix in layer_params:
         with tf.name_scope("Weights"):
-            a, w, b = make_fc_layer(name_postfix, a_prev, n_outputs, activation, initializer)
+            a, w, b = make_fc_layer(name_postfix, a_prev, n_inputs, n_outputs, activation, initializer)
             A.append(a)
             W.append(w)
             B.append(b)
@@ -163,10 +194,10 @@ def identity(arg):
     return arg
 
 
-def make_fc_layer(name_postfix, input, n_outputs, activation, initializer):
+def make_fc_layer(name_postfix, input, n_inputs, n_outputs, activation, initializer):
     w, b = initialize_layer_params(
         name_postfix=name_postfix,
-        n_inputs=input.shape[0],
+        n_inputs=n_inputs,
         n_outputs=n_outputs,
         initializer=initializer)
     a = activation(tf.add(tf.matmul(w, input), b))
